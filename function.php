98a1b51c5569ee41bc33b3a29db92c304168b216
swordfly1979 <7115670@qq.com>
2018-12-06 15:34:59 +0800

    111

--- function.php
+++ function.php
@@ -1,5 +1,14 @@
 <?php
-    protected function list_to_tree($list = '', $pk = 'id', $pid = 'pid', $child = '_child', $root = 0)
+/*
+ * 把返回的数据集转换成Tree
+ * @param array $list 要转换的数据集
+ * @param string $pk 数组主键
+ * @param string $pid 父id名
+ * @param string $child 子数组键名
+ * @return array
+ * @author 道法自然
+ */
+    function list_to_tree($list = '', $pk = 'id', $pid = 'pid', $child = '_child', $root = 0)
     {
         $list = array_combine(array_column($list, $pk), $list);
         $tree = [];
