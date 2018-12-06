1467269facac2df170335ad1feaa219db51cdc1f
swordfly1979 <7115670@qq.com>
2018-12-06 16:49:07 +0800

    111

--- function.php
+++ function.php
@@ -8,18 +8,42 @@
  * @return array
  * @author 道法自然
  */
-    function list_to_tree($list = '', $pk = 'id', $pid = 'pid', $child = '_child', $root = 0)
-    {
-        $list = array_combine(array_column($list, $pk), $list);
-        $tree = [];
-        foreach ($list as $key => $val) {
-            if ($val[$pid] == $root) {
-                $tree[] =  & $list[$key];
-            } else {
-                if ($list[$val[$pid]]) {
-                    $list[$val[$pid]][$child][] = $list[$key];
-                }
+function list_to_tree($list = '', $pk = 'id', $pid = 'pid', $child = '_child', $root = 0)
+{
+    $list = array_combine(array_column($list, $pk), $list);
+    $tree = [];
+    foreach ($list as $key => $val) {
+        if ($val[$pid] == $root) {
+            $tree[] =  & $list[$key];
+        } else {
+            if ($list[$val[$pid]]) {
+                $list[$val[$pid]][$child][] = $list[$key];
             }
         }
-        return $tree;
     }
+    return $tree;
+}
+/**
+ * 对查询结果集进行排序
+ * @access public
+ * @param array $list 查询结果
+ * @param string $field 排序的字段名
+ * @param array $sortby 排序类型
+ * asc正向排序 desc逆向排序 nat自然排序
+ * @return array
+ */
+function tree_to_list($tree, $child = '_child', &$list = array()) {
+  if (is_array($tree)) {
+      foreach ($tree as $key => $value) {
+          $refer = $value;
+          if($refer[$child]){
+              unset($refer[$child]);
+          }
+          $list[] = $refer;
+          if (isset($value[$child])) {
+              tree_to_list($value[$child], $child,$list);
+          }
+      }
+  }
+  return $list;
+}
